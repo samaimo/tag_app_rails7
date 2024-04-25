ce5b724aa4809ad01dbada1ddb931e67b5643411
samaimo <kii328815@gmail.com>
2024-04-26 08:25:18 +0900

    formオブジェクトの作成とバリデーションの記述

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -1,3 +1,15 @@
 class PostForm
   include ActiveModel::Model
+
+  #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
+  attr_accessor :text, :image
+
+  with_options presence: true do
+    validates :text
+    validates :image
+  end
+
+  def save
+    Post.create(text: text, image: image)
+  end
 end
