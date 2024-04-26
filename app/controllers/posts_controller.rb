2eea6751cb604f3e0f36a1a73514b8f8eac19729
samaimo <kii328815@gmail.com>
2024-04-27 08:43:25 +0900

    画像がnilの場合、既存の画像をセットする記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,9 @@ class PostsController < ApplicationController
     # paramsの内容を反映したインスタンスを生成する
     @post_form = PostForm.new(post_form_params)
 
+    # 画像を選択し直していない場合は、既存の画像をセットする
+    @post_form.image ||= @post.image.blob
+
     if @post_form.valid?
       @post_form.update(post_form_params, @post)
       redirect_to root_path
