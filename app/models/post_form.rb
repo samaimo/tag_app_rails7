3af21adc395c88e9bbb0f1844056ab36f776fa2b
samaimo <kii328815@gmail.com>
2024-04-27 08:37:41 +0900

    PostFormクラスで取り扱うPostモデルの属性を追加

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -2,7 +2,7 @@ class PostForm
   include ActiveModel::Model
 
   #PostFormクラスのオブジェクトがPostモデルの属性を扱えるようにする
-  attr_accessor :text, :image
+  attr_accessor :text, :image, :id, :created_at, :updated_at
 
   with_options presence: true do
     validates :text
@@ -16,5 +16,5 @@ class PostForm
   def update(params,post)
     post.update(params)
   end
-  
+
 end
