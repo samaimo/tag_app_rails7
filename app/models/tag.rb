608649f65fbdab8ed2ccfe4d11692339749b28bf
samaimo <kii328815@gmail.com>
2024-04-27 08:55:02 +0900

    バリデーションの記述

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ApplicationRecord
   # アソシエーション
   has_many :post_tag_relations
   has_many :posts, through: :post_tag_relations
+  # バリデーションの記述   #一意性の制約はモデル単位で設ける必要があるので、FormオブジェクトのPostFormクラスではなく、Tagモデルに記述する。
+  validates :tag_name,  uniqueness: true
 end
