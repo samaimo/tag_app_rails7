26754daf6dc10ae0386f7d481c80f4de6e19b0b5
samaimo <kii328815@gmail.com>
2024-04-29 16:39:39 +0900

    searchアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"])
+    render json:{ keyword: tag }
+  end
+
   def new
     @post_form = PostForm.new
   end
