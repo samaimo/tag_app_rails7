3db3e7be5231fe78b6a206e0690da181e3d08225
samaimo <kii328815@gmail.com>
2024-04-29 17:19:18 +0900

    直前の検索結果を消す

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -12,15 +12,16 @@ const tagNameInput = document.querySelector("#post_form_tag_name");
         // console.log("非同期通信成功");
         const tagName = XHR.response.keyword;
         const searchResult = document.getElementById("search-result");
+        searchResult.innerHTML = "";
         tagName.forEach((tag) => {
           const childElement = document.createElement("div");
           childElement.setAttribute("class", "child");
           childElement.setAttribute("id", tag.id);
-          childElement.innerHTML = tag.tag_name;
+          childElement.innerHTML = tag.name;
           searchResult.appendChild(childElement);
           const clickElement = document.getElementById(tag.id);
-          clickElement.addEventListener("click",() => {
-            document.getElementById("post_form_tag_name").value = clickElement. clickElement.textContent;
+          clickElement.addEventListener("click", () => {
+            document.getElementById("post_form_tag_name").value = clickElement.textContent;
             clickElement.remove();
           });
         });
