42b72b4b16e9a2e8d5d0e3e527abba6978ecd57f
samaimo <kii328815@gmail.com>
2024-04-29 17:26:26 +0900

    レスポンスにデータが存在する場合のみ処理を実行

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -9,21 +9,23 @@ const tagNameInput = document.querySelector("#post_form_tag_name");
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
-        const tagName = XHR.response.keyword;
         const searchResult = document.getElementById("search-result");
         searchResult.innerHTML = "";
-        tagName.forEach((tag) => {
-          const childElement = document.createElement("div");
-          childElement.setAttribute("class", "child");
-          childElement.setAttribute("id", tag.id);
-          childElement.innerHTML = tag.tag_name;
-          searchResult.appendChild(childElement);
-          const clickElement = document.getElementById(tag.id);
-          clickElement.addEventListener("click", () => {
-            document.getElementById("post_form_tag_name").value = clickElement.textContent;
-            clickElement.remove();
+        if (XHR.response) {
+          const tagName = XHR.response.keyword;
+          tagName.forEach((tag) => {
+            const childElement = document.createElement("div");
+            childElement.setAttribute("class", "child");
+            childElement.setAttribute("id", tag.id);
+            childElement.innerHTML = tag.tag_name;
+            searchResult.appendChild(childElement);
+            const clickElement = document.getElementById(tag.id);
+            clickElement.addEventListener("click", () => {
+              document.getElementById("post_form_tag_name").value = clickElement.textContent;
+              clickElement.remove();
           });
         });
+        };
       };
     });
   };
